9a4e15d603d53f56de40d25bc7205ec31a141c6b
junwu <junwu2012@hotmail.com>
2020-02-17 16:31:55 +0000

    add conditional, function, array, loop hash

--- precourse_recap.rb
+++ precourse_recap.rb
@@ -1,5 +1,28 @@
-conditional(if,else)
-function
-array
-hash
-loop
+#!/usr/bin/ruby
+#conditional(if,else)
+x = 1
+if x>0
+  puts "x is positive"
+else
+  puts "x is negitive"
+end
+#function
+def greet name
+  puts "hello #{name}"
+end
+
+greet "jun"
+#array
+my_hobbies = ["table tennis","joggy","badminton","swimming"]
+
+my_hobbies << "kongfu"
+#loop
+for hobby in my_hobbies
+  p hobby
+end
+#hash
+people = {"name" => "jun","city" => "Edinburgh","tel"=>"07877345678"}
+
+people["national"] = "China"
+
+puts people["national"]
